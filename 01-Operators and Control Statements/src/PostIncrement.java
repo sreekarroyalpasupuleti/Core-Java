85766917fe0946f28803516217fddd4237da963b
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-26 17:12:20 -0400

    Second Module

---
+++ 01-Operators and Control Statements/src/PostIncrement.java
@@ -0,0 +1,16 @@
+public class PostIncrement {
+
+        public static void main(String... args){
+
+            int a = 6;
+
+            System.out.println(a++); // it will print 6 then it will become 7
+
+            a++;  // it will become 8
+
+            System.out.println(a++); // it will print 8 then it will become 9
+
+            System.out.println(a); // it will print 9
+        }
+
+}
