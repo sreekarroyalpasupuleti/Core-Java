60b6ac2bb3b6f431fd8fe044b173177b4b24d260
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-26 20:10:27 -0400

    Variables are used to store the data

---
+++ 00-Basics of Java/src/VariablesProgram.java
@@ -0,0 +1,23 @@
+public class VariablesProgram {
+
+
+        public static void main (String... args) {
+
+            int salary = 77000;
+            System.out.println(salary);
+
+            float speed = 11.99f;
+            System.out.println(speed);
+
+            double customers = 120.87;
+            System.out.println(customers);
+
+            char gender = 'f';
+            System.out.println(gender);
+
+            boolean pass = true;
+            System.out.println(pass);
+
+        }
+
+}
