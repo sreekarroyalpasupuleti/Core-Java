b16f83d7e71ecd253791d65c2380727d7ee1d2c2
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-28 16:35:29 -0400

    Decrement

---
+++ 01-Operators and Control Statements/src/Decrement.java
@@ -0,0 +1,11 @@
+public class Decrement {
+
+
+        public static void main(String... args){
+            int a = 78;
+            System.out.println( a -- );  // it will print 78 then it will become 77
+            System.out.println( -- a);  // it will become 76 then it will print 76
+        }
+
+
+}
