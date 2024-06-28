7047441fd63fcc9997c17be9bcb770442926a8d0
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-28 01:06:05 -0400

    Pre Increment

---
+++ 01-Operators and Control Statements/src/PreIncrement.java
@@ -0,0 +1,16 @@
+public class PreIncrement {
+
+        public static void main(String... args){
+
+            int a = 78;
+            System.out.println ( ++ a );  // it will become 79 then it will print
+
+            ++ a ;  // it will become 80
+
+            System.out.println(++a);  // it will become 81 then it will print
+
+            System.out.println(a); // it will print 81
+        }
+
+
+}
