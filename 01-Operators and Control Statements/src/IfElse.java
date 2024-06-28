56485db7eced74daa55289b6544b615a32b72fc0
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-28 17:16:43 -0400

    IfElse

---
+++ 01-Operators and Control Statements/src/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+
+        public static void main (String... args){
+            int age = 56 ;
+
+            if ( age >= 18 ) {
+                System.out.println("Major") ;
+            } else {
+                System.out.println("Minor");
+            }
+        }
+}
