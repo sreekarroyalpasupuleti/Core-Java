2640ccdc565f9518ae3a8c7db81cbdcecfd88112
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-28 19:31:51 -0400

    Roles Finder

---
+++ 01-Operators and Control Statements/src/RoleFinder.java
@@ -0,0 +1,24 @@
+import java.io.*;
+  public class RoleFinder {
+
+        public static void main(String... args)  throws Exception {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            String str = br.readLine ( );
+            int salary = Integer.parseInt(str);
+
+            if( salary >= 10000 && salary <= 20000 ){
+                System.out.println("Fresher");
+            }else if ( salary >= 35000 &&  salary <=65000 ){
+                System.out.println("AI Engineer");
+            }else if( salary >= 96000 && salary <=190000 ){
+                System.out.println("Data Scientist");
+            }else if( salary >= 100000 && salary <=150000 ){
+                System.out.println("Business Analyst");
+            }else {
+                System.out.println("Cannot Found");
+            }
+
+        }
+
+}
