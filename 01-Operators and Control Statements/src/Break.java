ad087b82adb2200a6ab74eeb6614fe4b860f3ff1
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-29 13:36:28 -0400

    Break

---
+++ 01-Operators and Control Statements/src/Break.java
@@ -0,0 +1,13 @@
+//break  ====> It is used to come out from switch case and from loops
+public class Break {
+    public static void main(String... args){
+        for (int i = 77; i<= 92; i++ ){
+            if (i >= 91 ){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+
+
+}
