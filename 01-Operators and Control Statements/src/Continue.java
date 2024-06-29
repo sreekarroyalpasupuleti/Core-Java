a37b6b087e0dc1a3290699b885d3db59bc78cf8c
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-29 13:38:49 -0400

    Continue

---
+++ 01-Operators and Control Statements/src/Continue.java
@@ -0,0 +1,13 @@
+
+public class Continue {
+
+        public static void main(String... args){
+
+            for (int i = 77; i<= 92; i++ ){
+                if(i == 78 ) {
+                    continue;
+                }
+                System.out.println(i);
+            }
+        }
+}
