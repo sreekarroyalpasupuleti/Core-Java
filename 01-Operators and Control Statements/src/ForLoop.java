5706ec12f4602cee71ed133cc8896dd740d9a8bd
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-28 22:46:07 -0400

    For Loop

---
+++ 01-Operators and Control Statements/src/ForLoop.java
@@ -0,0 +1,19 @@
+// for loop
+// It is used to execute statements multiple times
+// For loop is called as Range based loop
+
+//syntax
+//        for ( initialization ; condition ; increment / decrement ) {
+
+        //stmts
+//        }
+
+// Q) Write java program to print numbers from 41 to 89 using for loop
+
+public class ForLoop {
+    public static void main(String... args){
+        for ( int i = 41 ; i <= 89 ; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
