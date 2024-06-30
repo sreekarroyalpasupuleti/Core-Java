bc92448f1895fa4f5ba90e7bd2768024daa712a3
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-29 23:16:57 -0400

    ArrayCreation

---
+++ 02-Arrays/src/ArrayCreation.java
@@ -0,0 +1,18 @@
+class ArrayCreation {
+
+    public static void main(String... args){
+
+        int [ ]  arr  = new int [ 4 ] ;
+
+        arr[0] = 121;
+        arr[1] = 122;
+        arr[2] = 123;
+        arr[3] = 103;
+
+        System.out.println ( arr [ 0 ] ) ;
+        System.out.println ( arr [ 1 ] ) ;
+        System.out.println ( arr [ 2 ] ) ;
+        System.out.println ( arr [ 3 ] ) ;
+
+    }
+}
