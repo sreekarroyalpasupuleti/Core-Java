c6bcf9b1411eac3ad7574ed6322affb049d5eba4
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-29 22:45:22 -0400

    Break

--- 01-Operators and Control Statements/src/Break.java
+++ 01-Operators and Control Statements/src/Break.java
@@ -1,7 +1,7 @@
 //break  ====> It is used to come out from switch case and from loops
 public class Break {
     public static void main(String... args){
-        for (int i = 77; i<= 92; i++ ){
+        for (int i = 62; i<= 92; i++ ){
             if (i >= 91 ){
                 break;
             }
