de3a58727d5a2d83b8eaab50efab431e01579c8e
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-30 00:15:20 -0400

    ArraysWthBoolean

---
+++ 02-Arrays/src/AraysWithBoolean.java
@@ -0,0 +1,15 @@
+public class AraysWithBoolean {
+
+        public static void main(String... args) {
+
+            boolean [ ]  arr  = new boolean [ 3 ] ;
+
+            arr [1] = true;
+            arr [2] = true;
+
+
+            for( int i = 0 ; i < arr.length ; i ++ ) {
+                System.out.println ( arr [i] );
+            }
+        }
+    }
