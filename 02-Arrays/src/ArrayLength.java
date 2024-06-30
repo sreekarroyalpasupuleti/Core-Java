6a8b8d95fb2dd7c29aec5e7284ecbef974901b76
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-29 23:25:31 -0400

    ArrayLength

---
+++ 02-Arrays/src/ArrayLength.java
@@ -0,0 +1,14 @@
+//We can find size of the array using length property
+
+public class ArrayLength {
+    public static void main(String... args){
+
+        int [ ]  arr  = new int [ 4 ] ;
+        arr[0] = 11;
+        arr[1] = 15;
+        arr[2] = 162;
+
+        System.out.println ( arr.length ) ;
+    }
+}
+
