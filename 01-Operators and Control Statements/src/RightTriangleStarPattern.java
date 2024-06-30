370dc72af7fa1a8e50e0e9974bdc54e2d0f978f5
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-29 21:17:02 -0400

    Right Triangle Star Pattern

--- 01-Operators and Control Statements/src/RightTriangleStarPattern.java
+++ 01-Operators and Control Statements/src/RightTriangleStarPattern.java
@@ -6,7 +6,7 @@ public class RightTriangleStarPattern {
 
             for( int i = 5; i<=10 ; i ++ ){
 
-                for( int j = 1; j<=i ; j++ ){
+                for( int j = 5; j<=i ; j++ ){
                     System.out.print ("* ");
                 }
 
