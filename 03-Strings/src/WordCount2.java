809b9837859a4443337e16b25c21bcef8504af79
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-07-02 23:09:26 -0400

    WordCount2

---
+++ 03-Strings/src/WordCount2.java
@@ -0,0 +1,12 @@
+public class WordCount2 {
+
+        public static void main(String... args){
+
+            String s = "Good Morning";
+
+            String[ ] arr = s.trim().split("\\s+");
+
+            System.out.println(arr.length);
+        }
+
+    }
