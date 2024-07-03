731ea3e04715ee647041e1dfadc5b2e258b95110
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-07-02 23:06:46 -0400

    WordCount

---
+++ 03-Strings/src/WordCount.java
@@ -0,0 +1,12 @@
+public class WordCount {
+
+        public static void main(String... args){
+
+            String s = "Happy Birthday my friend  .";
+
+            String[ ] arr = s.split("\\s+");
+
+            System.out.println(arr.length);
+        }
+
+    }
