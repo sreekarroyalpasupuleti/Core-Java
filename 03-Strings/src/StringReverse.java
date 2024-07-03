a27ce64df73bbf71cffe667003b34f442065f5c9
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-07-02 22:44:25 -0400

    ReverseArray

---
+++ 03-Strings/src/StringReverse.java
@@ -0,0 +1,18 @@
+public class StringReverse {
+
+        public static void main(String[ ] args){
+
+            String s = "Reverse";
+
+            String rev = "";
+
+            for( int i = s.length( ) - 1 ; i >=0 ;  i-- ){
+
+                rev = rev + s.charAt ( i ) ; //esreveR
+            }
+            System.out.println(rev);
+
+        }
+
+    }
+
